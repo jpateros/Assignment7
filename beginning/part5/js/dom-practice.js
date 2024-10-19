a2d2891765daee1a8e1d219a1b969524183d48db
jpateros <jpateros5410@sdsu.edu>
2024-10-19 13:11:42 -0700

    finsihed part 5 need to clean up

--- beginning/part5/js/dom-practice.js
+++ beginning/part5/js/dom-practice.js
@@ -1,12 +1,26 @@
 /*eslint-env browser*/
 
 //STEP 1
-
+function alertFunction () {
+   alert("I have been clicked");
+}
 //STEP 2
-
+window.onload = function() {
+    var button = document.getElementById("2049");
+    button.onclick = alertFunction;
+};
 //STEP 3
-
+window.onload = function() {
+    var button = document.getElementById("part3");
+    button.addEventListener("click", alertFunction);
+};
 //STEP 4
-
+window.onload = function() {
+    var button = document.getElementById("part4");
+  
+    button.addEventListener("click", function() {
+        alert("I have been clicked using callback function");
+    });
+};
 //STEP 5
-
+//inside of html file!!
