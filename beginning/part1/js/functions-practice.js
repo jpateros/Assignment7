323cd6e8227ec142eb74e405fb8fd2a383d8b6d4
jpateros <jpateros5410@sdsu.edu>
2024-10-19 10:21:16 -0700

    initial repository commit with beginning files

---
+++ beginning/part1/js/functions-practice.js
@@ -0,0 +1,15 @@
+//STEP 1
+function halfNumber(number) {
+    var result = number / 2;
+    console.log("Half of " + number + " is " + result + ".");
+    document.write("Half of " + number + " is " + result + ".");
+    return result;
+}
+halfNumber(2);
+//STEP 2
+
+//STEP 3
+
+//STEP 4
+
+//STEP 5
